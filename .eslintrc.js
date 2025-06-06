bb9135b051fbbf2e390c37cd3103b28c8a8ae6a4
Mredosz <mredosz4@gmail.com>
2025-06-06 10:35:36 +0200

    Test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-    env: {
-        browser: true,
-        node: true,
-        es2022: true,
-        jest: true
-    },
-    extends: ["eslint:recommended"],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
-    },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
 };
