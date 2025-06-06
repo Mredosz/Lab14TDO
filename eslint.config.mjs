192e64eafe0a1df10d348ba0194d61491990fac2
Mredosz <mredosz4@gmail.com>
2025-06-06 09:59:23 +0200

    Test

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "@eslint/js";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
