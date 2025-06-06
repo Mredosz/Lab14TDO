fe640bd3e9df41a4e30a718f17b97a85388d12a4
Mredosz <mredosz4@gmail.com>
2025-06-06 09:43:18 +0200

    Test

---
+++ integration-test.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+(async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/health');
+        if (response.status === 200) {
+            console.log('Integration test passed');
+        } else {
+            console.error('Integration test failed');
+            process.exit(1);
+        }
+    } catch (error) {
+        console.error('Integration test error:', error.message);
+        process.exit(1);
+    }
+})();
